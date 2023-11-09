4b2032fc28fe25f1f028d2f7bc124092ce63c933
dobinDev <ff5026@mym-b.com>
2023-11-09 12:51:38 +0900

    Initial commit

---
+++ src/test/java/blockchain/hyperledger/hyperledgerfabric/HyperledgerFabricApplicationTests.java
@@ -0,0 +1,13 @@
+package blockchain.hyperledger.hyperledgerfabric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperledgerFabricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
