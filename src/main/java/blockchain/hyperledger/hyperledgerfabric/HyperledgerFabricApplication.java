88796be5a8e7f5d1e589191f6067b3a3338f4edb
ff5026@mym-b.com <ff5026@mym-b.com>
2024-03-11 15:15:59 +0900

    :memo: add : gradle.build update ver.5

--- src/main/java/blockchain/hyperledger/hyperledgerfabric/HyperledgerFabricApplication.java
+++ src/main/java/blockchain/hyperledger/hyperledgerfabric/HyperledgerFabricApplication.java
@@ -2,8 +2,15 @@ package blockchain.hyperledger.hyperledgerfabric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        ContextInstanceDataAutoConfiguration.class,
+        ContextStackAutoConfiguration.class,
+        ContextRegionProviderAutoConfiguration.class
+})
 public class HyperledgerFabricApplication {
 
     public static void main(String[] args) {
