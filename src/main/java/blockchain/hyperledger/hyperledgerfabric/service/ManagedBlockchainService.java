1c0ab1f06683b06139aa557dc3f9ca7dfe718a62
ff5026@mym-b.com <ff5026@mym-b.com>
2024-03-14 17:34:30 +0900

    :memo: add : getToken code 추가.

--- src/main/java/blockchain/hyperledger/hyperledgerfabric/service/ManagedBlockchainService.java
+++ src/main/java/blockchain/hyperledger/hyperledgerfabric/service/ManagedBlockchainService.java
@@ -8,6 +8,10 @@ import com.amazonaws.services.managedblockchain.model.GetMemberResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @Service
 @RequiredArgsConstructor
 public class ManagedBlockchainService {
@@ -22,7 +26,41 @@ public class ManagedBlockchainService {
         return managedBlockchainClient.getMember(request);
     }
 
-    public String getToken(String tokneId) {
+    /*public String getToken(String tokneId) {
+
+        try {
+            // 실행할 명령어를 배열로 정의합니다.
+            String[] command = {};
+
+            // ProcessBuilder를 사용하여 외부 명령어를 실행합니다.
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+            // 프로세스를 시작하고 실행 결과를 받습니다.
+            Process process = processBuilder.start();
+
+            // 실행 결과를 읽어오기 위한 BufferedReader를 생성합니다.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            // 실행 결과를 읽어와 출력합니다.
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // 프로세스가 종료될 때까지 기다립니다.
+            process.waitFor();
+
+            // 프로세스의 종료 코드를 확인합니다.
+            int exitCode = process.exitValue();
+            System.out.println("프로세스 종료 코드 : " + exitCode);
+
+            // 리소스를 해제합니다.
+            reader.close();
+            process.destroy();
+        } catch (IOException | InterruptedException exception) {
+            exception.printStackTrace();
+        }
 
         String caFilePath = "/opt/home/managedblockchain-tls-chain.pem";
         String channelID = "mychannel";
@@ -34,9 +72,52 @@ public class ManagedBlockchainService {
                         "--name %s -c '{\"Args\":[\"GetToken\", \"%s\"]}'", caFilePath, channelID, chaincodeName, tokneId);
 
         return tokenID;
+    }*/
+
+    public String getToken(String tokenId) {
+        String caFilePath = "/opt/home/managedblockchain-tls-chain.pem";
+        String channelID = "mychannel";
+        String chaincodeName = "mycc";
+
+        return executeCommand(String.format("docker exec cli peer chaincode query " +
+                "--tls --cafile %s " +
+                "--channelID %s " +
+                "--name %s -c '{\"Args\":[\"GetToken\", \"%s\"]}'", caFilePath, channelID, chaincodeName, tokenId));
+    }
+
+    private String executeCommand(String command) {
+        StringBuilder output = new StringBuilder();
+
+        try {
+            // ProcessBuilder를 사용하여 외부 명령어를 실행합니다.
+            Process process = new ProcessBuilder("sh", "-c", command).start();
+
+            // 실행 결과를 읽어오기 위한 BufferedReader를 생성합니다.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            // 실행 결과를 읽어와 StringBuilder에 추가합니다.
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            // 프로세스가 종료될 때까지 기다립니다.
+            process.waitFor();
+
+            // 리소스를 해제합니다.
+            reader.close();
+            process.destroy();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
     }
 
     public String getAllTokens() {
+
+        ProcessBuilder builder = new ProcessBuilder();
 
         String caFilePath = "/opt/home/managedblockchain-tls-chain.pem";
         String channelID = "mychannel";
